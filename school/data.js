91cc6ad6987d4be74cb8f58aba3905b29d914e0e
abc1222322413 <shane981015@gmail.com>
2025-11-13 11:54:30 +0800

    67

---
+++ school/data.js
@@ -0,0 +1,216 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "川堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0957603315531212,
+          "pitch": 0.17552885839436172,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.9977506263917224,
+          "pitch": 0.10482930341374086,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.043972474082220714,
+          "pitch": 0.25274955602741045,
+          "title": "川堂",
+          "text": "附近有很多標語"
+        },
+        {
+          "yaw": 0.9363855980787292,
+          "pitch": 0.09141723804004087,
+          "title": "中庭",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.138803436947331,
+          "pitch": 0.0940930844984127,
+          "title": "中庭",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "中庭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6600937087277927,
+          "pitch": -0.7741056941781892,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.4836237244955113,
+          "pitch": 0.042838593834275684,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.19143272373173836,
+          "pitch": 0.43083639964241804,
+          "title": "噴水池",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.5961888292295026,
+          "pitch": 0.22164877361248791,
+          "title": "中庭",
+          "text": "學校有兩個長得一樣的中庭"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "司令台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7777003009667371,
+          "pitch": 0.20218776008354666,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3307261064631284,
+          "pitch": 0.00036474542057973736,
+          "title": "活動中心",
+          "text": "體育課、演講有時都會在這舉行"
+        },
+        {
+          "yaw": -1.9394018093031367,
+          "pitch": 0.21239967699841777,
+          "title": "游泳池",
+          "text": "游泳池也能從學校裡進入"
+        },
+        {
+          "yaw": -0.3557572116796983,
+          "pitch": 0.149280408132638,
+          "title": "炊事區",
+          "text": "童軍課會用到一次，可以煮東西"
+        },
+        {
+          "yaw": -0.9313553972038751,
+          "pitch": 0.21894360869958618,
+          "title": "操場",
+          "text": "朝會、體育課、校慶、運動會等，很多事情都會在這舉行，放學和下課都會有很多人"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6561571655643164,
+          "pitch": 0.1138679905840565,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
